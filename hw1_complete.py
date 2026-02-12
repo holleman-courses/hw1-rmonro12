1eb640b54cdf51f2307afde7bbdb2dc764333eff
Ryan Monroe <rmonro12@charlotte.edu>
2026-02-12 00:08:46 -0500

    Replaced input_shape

--- hw1_complete.py
+++ hw1_complete.py
@@ -27,7 +27,7 @@ def build_model1():
 
 def build_model2():
   model = tf.keras.Sequential ([
-    Input(shape=input_shape),
+    Input(shape=32, 32, 3),
     layers.Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same', strides=(2,2)),
     layers.BatchNormalization(),
     layers.Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same', strides=(2,2)),
@@ -47,7 +47,7 @@ def build_model2():
 
 def build_model3():
   model = tf.keras.Sequential ([
-    Input(shape=input_shape),
+    Input(shape=32, 32, 3),
     layers.SeparableConv2D(32, kernel_size=(3, 3), activation='relu', padding='same', strides=(2,2)),
     layers.BatchNormalization(),
     layers.SeparableConv2D(64, kernel_size=(3, 3), activation='relu', padding='same', strides=(2,2)),
@@ -67,7 +67,7 @@ def build_model3():
 
 def build_model50k():
   model = tf.keras.Sequential ([
-    Input(shape=input_shape),
+    Input(shape=32, 32, 3),
     layers.Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same', strides=(2,2)),
     layers.BatchNormalization(),
     layers.SeparableConv2D(64, kernel_size=(3, 3), activation='relu', padding='same'),
@@ -98,7 +98,6 @@ if __name__ == '__main__':
   train_images = train_images[trn_idxs, :,:,:]
   val_labels = train_labels[val_idxs]
   train_labels = train_labels[trn_idxs]
-  input_shape  = train_images.shape[1:]
   ########################################
   
   ### Build, compile, and train model 1
